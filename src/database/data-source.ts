96b3d90b8d37ede9279fbb84353f799e1e081026
purupurupu <purupurupu0prog@gmail.com>
2024-04-06 23:34:52 +0900

    コードの変更を記述するためのデータベースエンティティとデータソースを追加しました

---
+++ src/database/data-source.ts
@@ -0,0 +1,23 @@
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { ExpoDriver } from "typeorm/driver/expo/ExpoDriver";
+import { User } from "./entity/User";
+
+export const AppDataSource = new DataSource({
+  type: "expo",
+  database: "your-database-name.sqlite",
+  driver: ExpoDriver,
+  entities: [User],
+  synchronize: true,
+  logging: false,
+});
+
+AppDataSource.initialize()
+  .then(() => {
+    // データベースの初期化が成功した場合の処理
+    console.log("Data Source has been initialized!");
+  })
+  .catch((err) => {
+    // 初期化中にエラーが発生した場合の処理
+    console.error("Error during Data Source initialization:", err);
+  });
